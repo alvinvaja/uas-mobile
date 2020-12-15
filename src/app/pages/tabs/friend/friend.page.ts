95ae7fa4c5d6489cee3f282359b57372a135a91e
Alvin Valenciano <alvinvaja@gmail.com>
2020-12-16 00:46:10 +0700

    add friend page

--- src/app/pages/tabs/friend/friend.page.ts
+++ src/app/pages/tabs/friend/friend.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-friend',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend.page.scss'],
 })
 export class FriendPage implements OnInit {
+  user: User;
+  currentEmail: string;
+  friends: User[];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private db: AngularFirestore
+  ) { }
 
   ngOnInit() {
-  }
+    this.friends = [];
+    this.currentEmail = localStorage.getItem('email') !== null ? localStorage.getItem('email') : '';
+    this.user = { id: '', name: '', nim: '', email: '', lat: 0, long: 0, photo: '../../../assets/icon/avatar.svg'};
+    this.userService.getAllUsers().subscribe(res => {
+      this.friends = res;
+      // res.forEach(data => {
+      //   if (data.email === this.currentEmail) {
+      //     this.user = data;
 
+      //     const id = this.user.id;
+      //     const dataFriend = this.db.collection('users').doc(id).collection('friends').snapshotChanges().pipe(
+      //       map((actions) => {
+      //         return actions.map((a) => {
+      //           const data = a.payload.doc.data();
+      //           const id = a.payload.doc.id;
+      //           return { id, ...data };
+      //         });
+      //       })
+      //     );
+
+      //     dataFriend.pipe(take(1)).subscribe(friendID => {
+      //       this.friends = this.friends.filter((item) => {
+      //         return friendID.indexOf(item) !== -1;
+      //       });
+      //     });
+      //   }
+      // });
+    });
+  }
 }
